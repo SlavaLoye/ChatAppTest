ce93a81778ff0877225c0a5e114b95a75d7865dc
SlavaLoye <electromusiccompany@gmail.com>
2018-06-23 11:33:08 +0300

    add[Firebase]

--- ChatApp/ChatApp/Controllers/ViewController.swift
+++ ChatApp/ChatApp/Controllers/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Вячеслав Лойе on 22.06.2018.
 //  Copyright © 2018 Вячеслав Лойе. All rights reserved.
-//https://www.youtube.com/watch?v=pLyEA6PuqAU&index=1&list=PLZ7bsrwx1wnVtKmnf0SfKY_-yz0aWo2_z
+//
 
 import UIKit
 
